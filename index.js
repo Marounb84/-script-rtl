0b10aecb986296e8b0eb63a7a46aed3b6f080681
Marounb84 <marounbisharat@gmail.com>
2025-08-03 13:16:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ScriptEditor from "./scriptEditor";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ScriptEditor />);
